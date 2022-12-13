93f0060d8470df864f6e762c9966822e94ec7b30
Ivaylo Draganov <dragunoff@gmail.com>
2022-12-13 18:22:34 +0200

    Move store creation out of main function

--- openra_launcher/lib/main.dart
+++ openra_launcher/lib/main.dart
@@ -1,35 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:openra_launcher/injection.dart';
-import 'package:openra_launcher/store/app_state.dart';
-import 'package:openra_launcher/store/middleware.dart';
-import 'package:openra_launcher/store/reducer.dart';
-import 'package:openra_launcher/usecases/get_installed_mods.dart';
+import 'package:openra_launcher/store/store.dart';
 import 'package:openra_launcher/widgets/app.widget.dart';
-import 'package:redux/redux.dart';
-import 'package:redux_persist/redux_persist.dart';
-import 'package:redux_persist_flutter/redux_persist_flutter.dart';
 
 Future<void> main() async {
-  // Init dependency injection
   configureDependencies();
-
-  final persistor = Persistor<AppState>(
-    storage: FlutterStorage(
-        key: 'openra-launcher',
-        location: FlutterSaveLocation.sharedPreferences),
-    serializer: JsonSerializer<AppState>(AppState.fromJson),
-  );
-
-  final initialState = await persistor.load();
-
-  final Store<AppState> store = Store<AppState>(
-    appReducer,
-    initialState: initialState ?? AppState.initial(),
-    middleware: [
-      ...createMiddleware(getIt.get<GetInstalledMods>()),
-      persistor.createMiddleware()
-    ],
-  );
+  final store = await createStore();
 
   runApp(OpenRALauncher(store: store));
 }
---
+++ openra_launcher/lib/store/store.dart
@@ -0,0 +1,30 @@
+import 'package:openra_launcher/injection.dart';
+import 'package:openra_launcher/store/app_state.dart';
+import 'package:openra_launcher/store/middleware.dart';
+import 'package:openra_launcher/store/reducer.dart';
+import 'package:openra_launcher/usecases/get_installed_mods.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_persist/redux_persist.dart';
+import 'package:redux_persist_flutter/redux_persist_flutter.dart';
+
+Future<Store<AppState>> createStore() async {
+  final persistor = Persistor<AppState>(
+    storage: FlutterStorage(
+        key: 'openra-launcher',
+        location: FlutterSaveLocation.sharedPreferences),
+    serializer: JsonSerializer<AppState>(AppState.fromJson),
+  );
+
+  final initialState = await persistor.load();
+
+  final Store<AppState> store = Store<AppState>(
+    appReducer,
+    initialState: initialState ?? AppState.initial(),
+    middleware: [
+      ...createMiddleware(getIt.get<GetInstalledMods>()),
+      persistor.createMiddleware()
+    ],
+  );
+
+  return store;
+}
