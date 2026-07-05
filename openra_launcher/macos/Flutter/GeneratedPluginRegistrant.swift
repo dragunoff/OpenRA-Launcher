e376846aef7ad7e3347db03a19ca41f644886d4c
Ivaylo Draganov <dragunoff@gmail.com>
2026-07-06 00:30:20 +0300

    Run `flutter pub upgrade --major-versions`

--- openra_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
+++ openra_launcher/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
