052f1f62c0dea54a1e66203a0c79d3150466f897
Ivaylo Draganov <dragunoff@gmail.com>
2026-08-05 23:15:03 +0300

    Clear favorite mods that are no longer among the installed mods

--- openra_launcher/lib/store/favorite_mods/reducer.dart
+++ openra_launcher/lib/store/favorite_mods/reducer.dart
@@ -1,4 +1,5 @@
 import 'package:openra_launcher/store/favorite_mods/actions.dart';
+import 'package:openra_launcher/store/installed_mods/actions.dart';
 import 'package:redux/redux.dart';
 
 final Reducer<Set<String>> favoriteModsReducer = combineReducers([
@@ -6,7 +7,19 @@ final Reducer<Set<String>> favoriteModsReducer = combineReducers([
   TypedReducer<Set<String>, RemoveModFromFavoritesAction>(
           _removeModFromFavorites)
       .call,
+  TypedReducer<Set<String>, ModsLoadedAction>(_cleanMissingFavorites).call,
+  TypedReducer<Set<String>, ModsEmptyAction>(_clearFavorites).call,
 ]);
+
+Set<String> _cleanMissingFavorites(
+    Set<String> favoriteMods, ModsLoadedAction action) {
+  final existingModKeys = action.mods.map((mod) => mod.key).toSet();
+  return Set.unmodifiable(Set.from(favoriteMods)..retainAll(existingModKeys));
+}
+
+Set<String> _clearFavorites(Set<String> favoriteMods, ModsEmptyAction action) {
+  return const {};
+}
 
 Set<String> _removeModFromFavorites(
     Set<String> favoriteMods, RemoveModFromFavoritesAction action) {
---
+++ openra_launcher/test/store/favorite_mods/reducer_test.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:openra_launcher/features/installed_mods/domain/entities/mod.dart';
+import 'package:openra_launcher/store/favorite_mods/reducer.dart';
+import 'package:openra_launcher/store/installed_mods/actions.dart';
+
+void main() {
+  group('Favorite mods reducer', () {
+    final favoriteKey = 'favorite-mod-1.0.0';
+    final missingFavoriteKey = 'missing-mod-1.0.0';
+
+    final installedMod = Mod(
+      key: favoriteKey,
+      id: 'favorite-mod',
+      version: '1.0.0',
+      title: 'Favorite Mod',
+      launchPath: '/launch',
+      launchArgs: const [''],
+    );
+
+    test('should retain favorite mods when loaded mods still include them', () {
+      final state = {favoriteKey};
+      final action = ModsLoadedAction({installedMod});
+
+      final nextState = favoriteModsReducer(state, action);
+
+      expect(nextState, {favoriteKey});
+    });
+
+    test('should remove favorites that are no longer available after mods load',
+        () {
+      final state = {favoriteKey, missingFavoriteKey};
+      final action = ModsLoadedAction({installedMod});
+
+      final nextState = favoriteModsReducer(state, action);
+
+      expect(nextState, {favoriteKey});
+    });
+
+    test('should clear favorites when installed mods are empty', () {
+      final state = {favoriteKey};
+      final action = ModsEmptyAction();
+
+      final nextState = favoriteModsReducer(state, action);
+
+      expect(nextState, isEmpty);
+    });
+  });
+}
