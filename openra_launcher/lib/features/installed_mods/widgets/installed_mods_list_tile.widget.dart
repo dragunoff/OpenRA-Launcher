f51f6cd834cfa59b36598568b6f607a635e1433d
Ivaylo Draganov <dragunoff@gmail.com>
2026-08-18 23:15:54 +0300

    Extract mod launch button widget

--- openra_launcher/lib/features/installed_mods/widgets/installed_mods_list_tile.widget.dart
+++ openra_launcher/lib/features/installed_mods/widgets/installed_mods_list_tile.widget.dart
@@ -1,15 +1,12 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:openra_launcher/constants/app_constants.dart';
 import 'package:openra_launcher/features/installed_mods/domain/entities/mod.dart';
-import 'package:openra_launcher/features/installed_mods/services/mod_launch_service.dart';
 import 'package:openra_launcher/features/installed_mods/widgets/favorite_mod_button.widget.dart';
 import 'package:openra_launcher/features/installed_mods/widgets/mod_actions_menu_button.widget.dart';
+import 'package:openra_launcher/features/installed_mods/widgets/mod_launch_button.widget.dart';
 import 'package:openra_launcher/features/installed_mods/widgets/unhide_mod_button.widget.dart';
 import 'package:openra_launcher/widgets/mod_icon.widget.dart';
 import 'package:openra_launcher/features/installed_mods/widgets/mod_release_info_chips.widget.dart';
-import 'package:openra_launcher/injection.dart';
 
 class InstalledModsListTile extends StatefulWidget {
   const InstalledModsListTile({
@@ -28,32 +25,8 @@ class InstalledModsListTile extends StatefulWidget {
 }
 
 class _InstalledModsListTileState extends State<InstalledModsListTile> {
-  bool _isLaunching = false;
   bool _isHovered = false;
   bool _isMenuOpen = false;
-
-  Future<void> _launchMod() async {
-    try {
-      await getIt<ModLaunchService>().launch(widget.mod);
-    } on ModLaunchException catch (_) {
-      if (!mounted) {
-        return;
-      }
-
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Could not launch ${widget.mod.title}'),
-          behavior: SnackBarBehavior.floating,
-        ),
-      );
-    }
-  }
-
-  void _setIsLaunching(bool isStarting) {
-    setState(() {
-      _isLaunching = isStarting;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,22 +49,7 @@ class _InstalledModsListTileState extends State<InstalledModsListTile> {
       ModReleaseInfoChips(
         mod: widget.mod,
       ),
-      FilledButton.icon(
-          icon: const Icon(Icons.rocket),
-          onPressed: () async {
-            if (_isLaunching) {
-              return;
-            }
-
-            _setIsLaunching(true);
-            _launchMod().whenComplete(() =>
-                // NOTE: Artificial delay to give feedback that something is going on
-                Timer(
-                  const Duration(milliseconds: 500),
-                  () => _setIsLaunching(false),
-                ));
-          },
-          label: const Text('Launch')),
+      ModLaunchButton(mod: widget.mod),
       pinnedButton,
       actionsMenuButton,
     ];
---
+++ openra_launcher/lib/features/installed_mods/widgets/mod_launch_button.widget.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:openra_launcher/features/installed_mods/domain/entities/mod.dart';
+import 'package:openra_launcher/features/installed_mods/services/mod_launch_service.dart';
+import 'package:openra_launcher/injection.dart';
+
+class ModLaunchButton extends StatefulWidget {
+  const ModLaunchButton({Key? key, required this.mod}) : super(key: key);
+
+  final Mod mod;
+
+  @override
+  State<ModLaunchButton> createState() => _ModLaunchButtonState();
+}
+
+class _ModLaunchButtonState extends State<ModLaunchButton> {
+  bool _isLaunching = false;
+
+  Future<void> _launchMod() async {
+    try {
+      await getIt<ModLaunchService>().launch(widget.mod);
+    } on ModLaunchException catch (_) {
+      if (!mounted) {
+        return;
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Could not launch ${widget.mod.title}'),
+          behavior: SnackBarBehavior.floating,
+        ),
+      );
+    }
+  }
+
+  void _setIsLaunching(bool isStarting) {
+    setState(() {
+      _isLaunching = isStarting;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton.icon(
+        icon: const Icon(Icons.rocket),
+        onPressed: () async {
+          if (_isLaunching) {
+            return;
+          }
+
+          _setIsLaunching(true);
+          _launchMod().whenComplete(() =>
+              // NOTE: Artificial delay to give feedback that something is going on
+              Timer(
+                const Duration(milliseconds: 500),
+                () => _setIsLaunching(false),
+              ));
+        },
+        label: const Text('Launch'));
+  }
+}
